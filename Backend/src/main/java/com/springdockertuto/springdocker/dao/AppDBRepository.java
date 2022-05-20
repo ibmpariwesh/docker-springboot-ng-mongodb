0d54adeb20ebdbeb38e5a42e43370596668a2a45
ibmpariwesh <ibmpariwesh@gmail.com>
2022-05-20 08:16:09 +0530

    fixed

--- Backend/src/main/java/com/springdockertuto/springdocker/dao/AppDBRepository.java
+++ Backend/src/main/java/com/springdockertuto/springdocker/dao/AppDBRepository.java
@@ -5,5 +5,5 @@ import com.springdockertuto.springdocker.model.AppModel;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
 public interface AppDBRepository extends MongoRepository<AppModel, String> {
-
+    // @Query
 }
